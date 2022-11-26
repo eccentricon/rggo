53f2ffa39bd8eec2f8ddf799918cb83a3601d8bd
Ted Osborne <eccentricon@users.noreply.github.com>
2022-11-26 15:24:49 -0500

    + count bytes

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -9,20 +9,23 @@ import (
 )
 
 func main() {
-	// Define bool flag -l to count lines
 	lines := flag.Bool("l", false, "Count links")
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parse flags provided by user
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// Use a scanner to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
 	// Define the scanner split type to words (default is split by lines)
-	if !countLines {
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}	else if !countLines {
 		scanner.Split(bufio.ScanWords)
 	}
 
--- firstProgram/wc/main_test.go
+++ firstProgram/wc/main_test.go
@@ -7,9 +7,11 @@ import (
 
 // TestCountWords tests the count function set to count words
 func TestCountWords(t *testing.T) {
+	countLines := false
+	countBytes := false
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4        // expected
-	res := count(b, false) // actual (result)
+	res := count(b, countLines, countBytes) // actual (result)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
@@ -17,9 +19,23 @@ func TestCountWords(t *testing.T) {
 
 // TestCountLines tests the count function set to count lines
 func TestCountLines(t *testing.T) {
+	countLines := true
+	countBytes := false
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 	exp := 3
-	res := count(b, true)
+	res := count(b, countLines, countBytes)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+// TestCountBytes tests the count function set to count bytes
+func TestCountBytes(t *testing.T) {
+	countLines := false
+	countBytes := true
+	b := bytes.NewBufferString("word1 word2\n")
+	exp := 12
+	res := count(b, countLines, countBytes)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
